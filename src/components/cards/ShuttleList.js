d19e5408dafeb965138aba97a6fd1e2919439022
Angry Era <87531104+angryera@users.noreply.github.com>
2023-06-02 12:45:32 +0200

    free to use cards

--- src/components/cards/ShuttleList.js
+++ src/components/cards/ShuttleList.js
@@ -1,39 +1,91 @@
 import React, { useState } from 'react';
-import "./shuttle.scss"
+import './shuttle.scss';
 
-function ShuttleList({ items }) {
-    const [selectedItems, setSelectedItems] = useState([]);
-    const [availableItems, setAvailableItems] = useState(items);
+function ShuttleList({ web3Required, freeToUse, onSelectionChange }) {
+    const [selectedItems, setSelectedItems] = useState(freeToUse);
+    const [availableItems, setAvailableItems] = useState(web3Required);
 
-    const handleAddItemClick = (item) => {
-        setSelectedItems([...selectedItems, item]);
-        setAvailableItems(availableItems.filter((i) => i.properties.name.value !== item.properties.name.value));
+    const handleAddItemClick = item => {
+        const updatedSelectedItems = [
+            ...selectedItems,
+            { ...item, count: 1 },
+        ].sort((item1, item2) => {
+            return item1.properties.id?.value - item2.properties.id?.value;
+        });
+        setSelectedItems(updatedSelectedItems);
+        setAvailableItems(
+            availableItems.filter(
+                i => i.properties.name.value !== item.properties.name.value,
+            ),
+        );
+        onSelectionChange(updatedSelectedItems);
     };
 
-    const handleRemoveItemClick = (item) => {
-        setSelectedItems(selectedItems.filter((i) => i.properties.name.value !== item.properties.name.value));
-        setAvailableItems([...availableItems, item]);
+    const handleRemoveItemClick = (item, count) => {
+        const updatedSelectedItems = selectedItems.map(i => {
+            if (i.properties.name.value === item.properties.name.value) {
+                return { ...i, count };
+            }
+            return i;
+        });
+        setSelectedItems(updatedSelectedItems);
+        if (count === 0) {
+            setAvailableItems(
+                [...availableItems, item].sort((item1, item2) => {
+                    return item1.properties.id?.value - item2.properties.id?.value;
+                }),
+            );
+        }
+        onSelectionChange(updatedSelectedItems.filter(i => i.count > 0));
+    };
+
+    const handleCountChange = (event, item) => {
+        const updatedSelectedItems = selectedItems.map(i => {
+            if (i.properties.name.value === item.properties.name.value) {
+                return { ...i, count: parseInt(event.target.value) };
+            }
+            return i;
+        });
+        setSelectedItems(updatedSelectedItems);
+        onSelectionChange(updatedSelectedItems.filter(i => i.count > 0));
     };
 
     return (
         <div className="shuttle-list-container">
             <ul className="shuttle-list shuttle-list-available">
                 <li className="shuttle-list-header">Web3 Required NFT Cards</li>
-                {availableItems.map((item) => (
+                {availableItems.map(item => (
                     <li key={item.properties.name.value} onClick={() => handleAddItemClick(item)}>
-                        {(item.properties.id ? item.properties.id.value + ": " : "Avatar:") + item.name}
+                        <div className="shuttle-item-name">
+                            {item.properties.id ? item.properties.id.value + ': ' : 'Avatar: '}
+                            {item.name}
+                        </div>
                     </li>
                 ))}
             </ul>
             <ul className="shuttle-list shuttle-list-selected">
                 <li className="shuttle-list-header">Free To Use</li>
-                {selectedItems.map((item) => (
-                     <li key={item.properties.name.value} onClick={() => handleRemoveItemClick(item)}>
-                     {(item.properties.id ? item.properties.id.value + ": " : "Avatar:") + item.name}
-                 </li>
+                {selectedItems.map(item => (
+                    <li key={item.properties.name.value}>
+                        <div className="shuttle-item-name">
+                            {item.properties.id ? item.properties.id.value + ': ' : 'Avatar: '}
+                            {item.name}
+                        </div>
+                        <div className="shuttle-item-count">
+                            <span onClick={() => handleRemoveItemClick(item, item.count - 1)}>−</span>
+                            <input
+                                type="number"
+                                min="0"
+                                max="3"
+                                value={item.count}
+                                onChange={event => handleCountChange(event, item)}
+                            />
+                            <span onClick={() => handleRemoveItemClick(item, item.count + 1)}>+</span>
+                        </div>
+                    </li>
                 ))}
             </ul>
-        </div >
+        </div>
     );
 }
 
--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { } from '../../actions/';
 import { HOST_URL } from '../../actions/types';
@@ -12,14 +14,48 @@ const CardsComponent = () => {
     const [selectedItems, setSelectedItems] = useState([]);
 
     useEffect(() => {
-        axios.get(HOST_URL + `getMetaData`).then(res => {
-            setAvailableItems(Object.values(res.data.metadatas));
-            console.log(res.data.metadatas);
-        })
+        axios.get(HOST_URL + `cardsInfo`).then(res => {
+            let cardInfos = res.data.cards;
+            cardInfos = cardInfos.filter((card) => card.is_free === 1);
+
+            axios.get(HOST_URL + `getMetaData`).then(res => {
+                const metadatas = Object.values(res.data.metadatas);
+                let web3Requried = [], freeToUse = [];
+
+                metadatas.forEach((metadata) => {
+                    let card = cardInfos.find((card) => card.id === metadata.properties.id?.value);
+                    if (!!card) {
+                        freeToUse.push({
+                            ...metadata,
+                            count: card.free_cards_count
+                        });
+                    } else {
+                        web3Requried.push(metadata);
+                    }
+                })
+
+                setAvailableItems(web3Requried);
+                setSelectedItems(freeToUse);
+
+            });
+        });
     }, []);
 
     const handleApplyClick = () => {
-        console.log(selectedItems);
+        const cardIds = selectedItems.map((item) => {
+            return {
+                id: item.properties.id?.value,
+                count: item.count,
+            }
+        });
+
+        axios.put(HOST_URL + 'cardsInfo', {
+            cardIds: cardIds
+        }).then(res => {
+            toast.success("Successfully updated free to use cards info");
+        }).catch(err => {
+            toast.error(err);
+        });
     };
 
     return (
@@ -30,12 +66,18 @@ const CardsComponent = () => {
                 <div className="content-wrapper" style={{ 'padding': '20px', 'borderTop': '0px' }}>
                     <div className="card card-default">
                         <div className="content-wrapper">
-                            {availableItems.length > 0 ? <ShuttleList items={availableItems} onSelectionChange={setSelectedItems} /> : <div>Loading...</div>}
+                            {availableItems.length > 0 ? <ShuttleList web3Required={availableItems} freeToUse={selectedItems} onSelectionChange={setSelectedItems} /> : <div>Loading...</div>}
                         </div>
                         <button className="btn btn-primary m-2" onClick={handleApplyClick}>Apply</button>
                     </div>
                 </div>
             </section>
+            <ToastContainer
+                autoClose={3000}
+                hideProgressBar={true}
+                theme="colored"
+                position="bottom-right"
+            />
         </>
     );
 }
